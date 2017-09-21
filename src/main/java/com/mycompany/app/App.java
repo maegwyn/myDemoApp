724ac8c379c1aab091e36df16bb1ddf11c1c11b2
Ibrahim Burak Sahin <burakshn96@gmail.com>
2017-09-21 21:31:33 +0300

    Changed the App.java second time

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -25,7 +25,7 @@ public class App
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
 
-        //get("/", (req, res) -> "Hello, World");
+        get("/", (req, res) -> "Hello, World");
 
         post("/compute", (req, res) -> {
           System.out.println(req.queryParams("input1"));
