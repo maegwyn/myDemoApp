8944d7d76549428e0727d78e35b80b8d69d0c3b5
Ibrahim Burak Sahin <burakshn96@gmail.com>
2017-09-21 22:15:38 +0300

    Added my algorithm

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,14 +12,22 @@ import spark.template.mustache.MustacheTemplateEngine;
 
 public class App
 {
-    public static boolean search(ArrayList<Integer> array, int e) {
+    public static ArrayList<String> myMethod(ArrayList<String> array, int e, ArrayList<String> a) {
       System.out.println("inside search");
-      if (array == null) return false;
+      if (array == null){
+	  	for(int i = 0;i<a.length;i++)
+			array.add(a.get(i));
+	  }
 
+		int count = 0;
+	  for(int elt2 : array){
+		  count++;
+	  }
+		
       for (int elt : array) {
-        if (elt == e) return true;
+        if (count == e) array.add("TRUE");
       }
-      return false;
+      return array;
     }
 
     public static void main(String[] args) {
@@ -30,26 +38,45 @@ public class App
         post("/compute", (req, res) -> {
           System.out.println(req.queryParams("input1"));
           System.out.println(req.queryParams("input2"));
+          System.out.println(req.queryParams("input3"));
 
+			
           String input1 = req.queryParams("input1");
           java.util.Scanner sc1 = new java.util.Scanner(input1);
           sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          java.util.ArrayList<String> inputList = new java.util.ArrayList<>();
           while (sc1.hasNext())
           {
-            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            String value = sc1.next().replaceAll("\\s","");
             inputList.add(value);
           }
           System.out.println(inputList);
 
+          String input3 = req.queryParams("input3");
+          java.util.Scanner sc3 = new java.util.Scanner(input3);
+          sc1.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<String> inputList3 = new java.util.ArrayList<>();
+          while (sc1.hasNext())
+          {
+            String value3 = sc1.next().replaceAll("\\s","");
+            inputList3.add(value);
+          }
+          System.out.println(inputList3);
+			
+			
 
           String input2 = req.queryParams("input2").replaceAll("\\s","");
           int input2AsInt = Integer.parseInt(input2);
 
-          boolean result = App.search(inputList, input2AsInt);
+          ArrayList <String> fullList = App.myMethod(inputList, input2AsInt,inputList3);
 
+		  boolean result = false;
+		  if(fullList.get(fullList.size()-1) == true)	
+			result = true;
+			
          Map map = new HashMap();
           map.put("result", result);
+		  map.put("Full List: ",fullList);	
           return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
